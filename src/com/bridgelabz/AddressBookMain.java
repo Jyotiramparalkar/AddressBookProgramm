16835854bdd5a793092e8c9a60893795c6c4a093
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-19 23:04:12 +0530

    Refactor AddressBook Programm using getter setter method

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -4,60 +4,32 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		AddressBook addressBook1 = new AddressBook();
-		
-		String firstName1,lastName1, address1, city1, state1, zip1, phoneNo1, email1;
-		
-		//Adding 1st person
-		System.out.println("Enter first name, last name, address, city, state, zip, phone number, email ID sequentially");
-		firstName1 = input.nextLine();
-		lastName1 = input.nextLine();
-		address1 = input.nextLine();
-		city1 = input.nextLine();
-		state1 = input.nextLine();
-		zip1 = input.nextLine();
-		phoneNo1 = input.nextLine();
-		email1 = input.nextLine();		
-			
-		Contact personObj1 =  new Contact(firstName1, lastName1, address1, city1, state1, zip1, phoneNo1, email1);
-		addressBook1.addToAddressBook(personObj1);
-		
-		personObj1.showdetails();
-		
-		//Adding 2nd person
-		System.out.println("Enter first name, last name, address, city, state, zip, phone number, email ID sequentially");
-		firstName1 = input.nextLine();
-		lastName1 = input.nextLine();
-		address1 = input.nextLine();
-		city1 = input.nextLine();
-		state1 = input.nextLine();
-		zip1 = input.nextLine();
-		phoneNo1 = input.nextLine();
-		email1 = input.nextLine();
-		
-		Contact personObj2 =  new Contact(firstName1, lastName1, address1, city1, state1, zip1, phoneNo1, email1);
-		addressBook1.addToAddressBook(personObj2);
-			
-		personObj2.showdetails();
-		
-		//Edit email id for given name
-		System.out.println("Enter name to edit person details");
-		String name=input.nextLine();
-		addressBook1.editPersonEmailId(name);
-		
-		System.out.println("Data after edit");
-		personObj1.showdetails();
-		personObj2.showdetails();
-		
-		input.close();
-	}
+    public static void main(String[] args) {
+    
+        System.out.println("Welcome to Address Book Program");
+        Scanner sc = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
 
-	
-
+        while (true){
+            System.out.println("choose a case ::");
+            int ch = sc.nextInt();
+            switch (ch){
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.displayContact();
+                    break;
+                case 3:
+                    addressBook.editContact();
+                    break;
+                default:
+                    System.out.println("Invalid Input");
+            }
+        }
+       
+    }
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -1,44 +1,103 @@
 package com.bridgelabz;
 
-public class Contact 
-{
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNo;
+    private String email;
 
-	String firstName;
-	String lastName;
-	String address;
-	String city;
-	String state;
-	String zip;
-	String phoneNumber;
-	String emailId;	
+    public Contact(){};
 
-	//Parameterized Contact constructor
-	public Contact(String firstName, String lastName, String address, String city, String state, String zip,
-			String phoneNumber, String emailId) 
-	{
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-		this.emailId = emailId;
-	}
+    public Contact(String firstName, String lastName, String street, String city, String state, int zip, long phoneNo, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
 
-	//Method for showing details
-	public void showdetails() 
-	{
-		System.out.println("First Name : "+firstName);
-		System.out.println("Last Name : "+lastName);
-		System.out.println("Address : "+address);
-		System.out.println("City : "+city);
-		System.out.println("State : "+state);
-		System.out.println("Zip : "+zip);
-		System.out.println("Phone Number : "+phoneNumber);
-		System.out.println("Email Id : "+emailId);
-	}
-	
-	
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNo=" + phoneNo +
+                ", email='" + email + '\'' +
+                '}'+"\n";
+    }
 }
